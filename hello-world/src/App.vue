ae4855f6380ebd1955bbede099a3c97d04e8f1c5
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 20:54:57 +0300

    v-once

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,46 +1,7 @@
 <template>
   <div>
-    <label>
-      <!-- v-model .lazy ile birlikte kullanıldığında işlemin bitmesi beklenir -->
-      <!-- v-model 'da her şey string olarak algılanır .number ile değiştirebiliriz -->
-      <!-- .trim boşlukları siler -->
-      <input v-model.lazy="message" placeholder="edit me">
-      <input v-model.number="number">
-      <textarea v-model="area"></textarea>
-      <input type="checkbox" v-model="checked">
-      <label>
-        <select v-model="selected">
-          <option disabled>select one</option>
-          <option>A</option>
-          <option>B</option>
-          <option>C</option>
-          <option>D</option>
-        </select>
-      </label>
-
-      <label>
-        <select v-model="selected2">
-          <option v-for="(item,index) in items" v-bind:value="item.value" v-bind:key="index">{{ item.option }}</option>
-        </select>
-      </label>
-
-      <div>
-        <input type="radio" value="1" id="1" v-model="radio">
-        <label for="1">1</label>
-        <input type="radio" value="2" id="2" v-model="radio">
-        <label for="2">2</label>
-        <input type="radio" value="3" id="3" v-model="radio">
-        <label for="3">3</label>
-      </div>
-
-    </label>
-    <p>{{ message }}</p>
-    <p>{{ area }}</p>
-    <p>{{ checked }}</p>
-    <p>{{ selected }}</p>
-    <p>{{ typeof (number) }}</p>
-    <p>{{ selected2 }}</p>
-    <p>{{ radio }}</p>
+    <!-- v-once ile ilk render sabit kalır , değişken değişse de render sabit kalır -->
+    <button v-once v-on:click="changeValue">{{ value }}</button>
   </div>
 </template>
 
@@ -48,21 +9,15 @@
 export default {
   data() {
     return {
-      message: '',
-      area: '',
-      checked: '',
-      selected: null,
-      selected2: null,
-      radio: null,
-      number: 0,
-      items: [
-        {option: 'A', value: 'one'},
-        {option: 'B', value: 'two'},
-        {option: 'C', value: 'three'},
-      ]
+      value: true
     }
   },
-  methods: {}
+  methods: {
+    changeValue() {
+      this.value = !this.value;
+      console.log(this.value);
+    }
+  }
 }
 </script>
 
