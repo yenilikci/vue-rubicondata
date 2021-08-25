20e605d7e34b200daed2eaf75a32bd4eb176e567
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 18:01:56 +0300

    v-if, v-show

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,10 +2,12 @@
   <div>
     <label>
       <!-- v-bind ile html attributelerine dinamik olarak değer verebiliriz, bu değerler ya data ya computed'dan gelmeli -->
-      <input v-bind:placeholder="inputHolder" v-on:change="changeInput($event)">
-      <input v-bind:maxlength="maxValue">
+      <input v-bind:placeholder="inputHolder" v-on:change="changeInput($event)" v-if="see">
+      <input v-bind:maxlength="maxValue" v-else>
       <!-- v-on ile html içerisindeki eventleri yakalarız , metotları da çalıştırabiliyoruz -->
-      <button v-on:click="buttonClick">Tıkla</button>
+      <button v-on:click="buttonClick" v-if="tryIt()">Tıkla</button>
+      <input placeholder="input 1" v-show="see">
+      <!-- v-show DOM'un her zaman üzerindedir , v-if v-else 'de ise her eklenen element silinir ve tekrar eklenir -->
     </label>
   </div>
 </template>
@@ -16,7 +18,8 @@ export default {
   data() {
     return {
       inputHolder: 'try it again',
-      maxValue: 10
+      maxValue: 10,
+      see: true
     }
   },
   methods: {
@@ -25,6 +28,13 @@ export default {
     },
     buttonClick() {
       console.log("clicked");
+    },
+    tryIt() {
+      if (this.see === true) {
+        return false;
+      } else {
+        return true;
+      }
     }
   }
 }
