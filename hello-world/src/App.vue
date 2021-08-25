e2794f5ac1f2fe87f7d0e1ba434f202589708c26
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 17:45:38 +0300

    v-on

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,8 +2,10 @@
   <div>
     <label>
       <!-- v-bind ile html attributelerine dinamik olarak değer verebiliriz, bu değerler ya data ya computed'dan gelmeli -->
-      <input v-bind:placeholder="inputHolder">
+      <input v-bind:placeholder="inputHolder" v-on:change="changeInput($event)">
       <input v-bind:maxlength="maxValue">
+      <!-- v-on ile html içerisindeki eventleri yakalarız , metotları da çalıştırabiliyoruz -->
+      <button v-on:click="buttonClick">Tıkla</button>
     </label>
   </div>
 </template>
@@ -15,6 +17,14 @@ export default {
     return {
       inputHolder: 'try it again',
       maxValue: 10
+    }
+  },
+  methods: {
+    changeInput($event) {
+      console.log($event.target.value);
+    },
+    buttonClick() {
+      console.log("clicked");
     }
   }
 }
