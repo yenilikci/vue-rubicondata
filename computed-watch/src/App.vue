656d33f47dfac9bc4fc239d69f67875e6a1dafad
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 23:00:05 +0300

    computed properties oluşturma

---
+++ computed-watch/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <p>{{ reverseMsg }}</p> <!-- bir sefer çalışır diğer seferlerde datayı cache serverdan döner -->
+    <p>{{ reverseMsgFunc() }}</p> <!-- methods kısmında tanımlanan her çağrıldığında yeniden bu fonksiyonu çağırır -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'this is message'
+    }
+  },
+  methods: {
+    reverseMsgFunc() {
+      return this.msg.split('').reverse().join('');
+    }
+  },
+  computed: {
+    reverseMsg() {
+      return this.msg.split('').reverse().join('');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
