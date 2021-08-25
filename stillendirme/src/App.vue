6aa980f21b2e721af21c4afcab64db7c3d4877b6
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 23:38:57 +0300

    dinamik olarak satır içi stillendirme

--- stillendirme/src/App.vue
+++ stillendirme/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <StaticStyle/>
+  <hr>
   <DynamicStyle/>
+  <hr>
+  <InlineDynamicStyle/>
 </template>
 
 <script>
 import StaticStyle from "./components/StaticStyle";
 import DynamicStyle from "./components/DynamicStyle";
+import InlineDynamicStyle from "./components/InlineDynamicStyle";
 
 export default {
   name: 'App',
   components: {
     StaticStyle,
-    DynamicStyle
+    DynamicStyle,
+    InlineDynamicStyle
   }
 }
 </script>
---
+++ stillendirme/src/components/InlineDynamicStyle.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div :style="styleObj">
+    this is try
+  </div>
+
+  <div :style="{color:activeColor,fontSize: fontSize1}">
+    this is try
+  </div>
+
+  <div :style="[styleObj,styleObj2]">
+    this is try
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "InlineDynamicStyle",
+  data() {
+    return {
+      activeColor: 'blue',
+      fontSize1: '20px',
+      styleObj: {
+        fontSize: '34px',
+        color: 'red',
+        fontStyle: 'italic'
+      },
+      styleObj2: {
+        fontSize: '34px',
+        color: 'yellow'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
