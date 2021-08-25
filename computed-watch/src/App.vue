aea4e4e570525cf2fced985a14827b3e113f0cbe
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 23:10:42 +0300

    watch properties ve termometre örneği

--- computed-watch/src/App.vue
+++ computed-watch/src/App.vue
@@ -4,10 +4,16 @@
     <p>{{ reverseMsg }}</p> <!-- bir sefer çalışır diğer seferlerde datayı cache serverdan döner -->
     <p>{{ reverseMsgFunc() }}</p> <!-- methods kısmında tanımlanan her çağrıldığında yeniden bu fonksiyonu çağırır -->
   </div>
+  <watch></watch>
 </template>
 
 <script>
+import Watch from "./Watch";
+
 export default {
+  components: {
+    Watch
+  },
   data() {
     return {
       msg: 'this is message'
---
+++ computed-watch/src/Watch.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <label>
+      Celcius:
+      <input type="text" v-model="cel">
+    </label>
+    <label>
+      Fahrenheit:
+      <input type="text" v-model="fah">
+    </label>
+    <label>
+      Kelvin:
+      <input type="text" v-model="kel">
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cel: 0,
+      fah: 0,
+      kel: 0
+    }
+  },
+  methods: {},
+  watch: {
+    // her inputun değişimini izlemek
+    // watch genelde asenkron ve büyük işlemler için kullanılır
+    // watchte tanımlanınca , değişkenler her değiştiğinde ilgili fonksiyonlar tekrar çalışır
+    cel(val) {
+      this.cel = val;
+      this.fah = (val * 1.8) + 32;
+      this.kel = val + 273.15;
+    },
+    fah(val) {
+      this.cel = (val - 32) / 1.8;
+      this.fah = val;
+      this.kel = ((val - 32) / 1.8) + 273.15;
+    },
+    kel(val) {
+      this.cel = val - 273.15;
+      this.fah = ((val - 273.15) * 1.8) + 32;
+      this.kel = val;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
