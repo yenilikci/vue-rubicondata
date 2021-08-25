8c76a879c0e3374b233b8c8555c520f3c484ccc8
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-25 22:05:12 +0300

    custom directive oluşturmak

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,45 +1,37 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(item,index) in items" :key="index">Product Name: {{ item.type }} Stock Quantity: {{
-          item.number
-        }}
-      </li>
-      <button @click="tryArray()">Add</button>
-    </ul>
+  <div id="app">
+    <span v-colorchange:bold.border="'red'">try</span> <br>
+    <span v-colorchange="'yellow'">try</span> <br>
+    <span v-colorchange="{color:'green',fontsize:''}">try</span>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      items: [
-        {type: 'pen', number: 33},
-        {type: 'ball', number: 344},
-        {type: 'rocket', number: 563},
-        {type: 'guitar', number: 64},
-        {type: 'table', number: 12},
-      ]
-    }
+    return {}
   },
-  methods: {
-    tryArray() {
-      //mutation fonk (dizi değerini değiştirir)
-      //this.items.push({type: 'pencil', number: 888})
-      //this.items.unshift({type: 'pencil', number: 888})
-      //const a = this.items.pop()
-      //const a = this.items.shift()
-      //const a = this.items.splice(1, 2) //start,deleteCount
-      //const a = this.items.splice(1, 2, {type: 'pencil', number: 888}); //3.param=items eklenir!
-      //diğerleri
-      //const a = this.items.concat({type: 'pencil', number: 888});
-      //const a = [...this.items, {type: 'pencil', number: 888}]; //spread op. -> sağda yazılan kısım override olur
-      //console.log(a)
-      //const a = this.items[2] = {type: 'pencil', number: 888};
+  directives: {
+    colorchange: {
+      mounted(el, binding) {
+        console.log(binding)
+        console.log("*************")
+        console.log(binding.name)
+        if (typeof el === "object") {
+          el.style.color = binding.value.color
+        }
+        el.style.color = binding.value
+        console.log("directive work");
+        if (binding.modifiers.border) {
+          el.style.border = 'thick solid #0000ff'
+          console.log("directive work white modifiers");
+        }
+        if (binding.arg.bold) {
+          el.style.fontWeight = '600';
+        }
+      }
     }
   }
-
 }
 </script>
 
