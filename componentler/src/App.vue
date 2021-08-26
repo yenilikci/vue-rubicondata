81c4e4fa73c22b28e86baf62656e9e70a32bdbee
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-26 11:08:56 +0300

    component oluşturma ve props ile veri gönderimi

--- componentler/src/App.vue
+++ componentler/src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <HelloWorld/>
-  <hello-world/>
+  <post :content="posts"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from "./components/Post";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      posts: [
+        {number: 1, content: 'one'},
+        {number: 2, content: 'two'},
+        {number: 3, content: 'three'},
+        {number: 4, content: 'four'},
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Post
   }
 }
 </script>
---
+++ componentler/src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h6 v-for="(i,index) in content" :key="index">
+      {{ i.number }} {{ i.content }}
+    </h6>
+    <button @click="addNewContent()">add new object</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: Array
+  },
+  methods: {
+    addNewContent() {
+      this.content.push({number: 5, content: 'five'})
+    }
+  }
+}
+</script>
