066f4cbb98a4471ecc59fb82b0f222ef742120bd
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-26 10:56:47 +0300

    component tanımı ve single file template component

---
+++ componentler/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld/>
+  <hello-world/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ componentler/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>this is component</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
