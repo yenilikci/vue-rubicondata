c26136d1ec3160cb6b9cbe3871527006a2a5f84b
yenilikci <m.melihcelik017@outlook.com>
2021-08-28 15:30:11 +0300

    filters

--- mixins-filters/src/App.vue
+++ mixins-filters/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
-    <button @click="hello">click</button>
-    {{ say }}
+    {{ 'hello' | capitalize }} <br>
+    {{ 'hello' | reverseValue }} <br>
+    {{ 'hello' | reverseValue | capitalize }} <br>
+    {{ 'hi' | newCapitalize }}
   </div>
 </template>
 
 <script>
-import {newMixin} from "../newMixin";
-
 export default {
-  mixins: [newMixin],
   data() {
-    return {
-      say: 'comp'
+    return {}
+  },
+  filters: {
+    capitalize(value) {
+      if (!value) ''
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+    reverseValue(value) {
+      return value.split('').reverse().join('');
     }
   }
 }
 </script>
 
 <style>
+
 </style>
--- mixins-filters/src/main.js
+++ mixins-filters/src/main.js
@@ -4,6 +4,12 @@ import {newMixin} from "../newMixin";
 
 Vue.config.productionTip = false
 
+Vue.filter('newCapitalize', (value) => {
+    if (!value) ''
+    value = value.toString();
+    return value.charAt(0).toUpperCase() + value.slice(1);
+})
+
 new Vue({
     mixins: [newMixin], //global mixin kullanımı
     render: h => h(App),
