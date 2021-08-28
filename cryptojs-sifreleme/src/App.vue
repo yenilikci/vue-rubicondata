601593906ed432a51bbfe4d4a17b5e7d63450b33
yenilikci <m.melihcelik017@outlook.com>
2021-08-28 13:02:42 +0300

    cryptojs ile şifreleme

---
+++ cryptojs-sifreleme/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input v-model="value" /> <br />
+    <button @click="saveData">save</button>
+  </div>
+</template>
+
+<script>
+var CryptoJS = require("crypto-js");
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    saveData() {
+      //encode
+      const data = CryptoJS.AES.encrypt(
+        this.value,
+        "yenilikci 123321444"
+      ).toString();
+      localStorage.setItem("userData", data);
+      console.log(data);
+
+      //decode
+      var bytes = CryptoJS.AES.decrypt(data, "yenilikci 123321444");
+      var originalText = bytes.toString(CryptoJS.enc.Utf8);
+      console.log(originalText);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
