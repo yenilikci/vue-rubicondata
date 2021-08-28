61c67ba4f039730d28509664cda2f315405eda81
yenilikci <m.melihcelik017@outlook.com>
2021-08-29 00:52:21 +0300

    animasyon - animation - bounce

--- animasyonlar/src/App.vue
+++ animasyonlar/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <button @click="show = !show">Toggle</button>
-    <transition name="fade">
-      <span v-if="show">Hello</span>
+    <!-- appear , appear-class ... -->
+    <!-- :duration="{ enter: 2000, leave: 1000 } -->
+    <transition name="bounce">
+      <p v-if="show">
+        Hello, Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+        reprehenderit iusto at illum beatae. Voluptatum magnam deserunt, minima,
+        amet labore laborum voluptates, natus nam blanditiis quae id ea a
+        excepturi.
+      </p>
     </transition>
   </div>
 </template>
@@ -18,12 +25,22 @@
 </script>
 
 <style>
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
+  .bounce-enter-active {
+    animation: bounce-in 0.5s;
   }
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 1s;
+  .bounce-leave-active {
+    animation: bounce-in 0.5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
