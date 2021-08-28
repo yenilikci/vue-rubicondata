8a31b493ba7d4fa00013c82f7dadfe0b1cae9dfe
yenilikci <m.melihcelik017@outlook.com>
2021-08-29 00:44:29 +0300

    animasyon - transition - fade

---
+++ animasyonlar/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="show = !show">Toggle</button>
+    <transition name="fade">
+      <span v-if="show">Hello</span>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: true,
+      };
+    },
+  };
+</script>
+
+<style>
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1s;
+  }
+</style>
